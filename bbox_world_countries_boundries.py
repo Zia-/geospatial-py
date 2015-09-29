c3af4c7bbabaeab94e381034f2a1620a62b3e1c1
Mohammed Zia <mohammed.zia33@gmail.com>
2015-09-29 15:52:24 +0300

    Add area calculation def

--- bbox_world_countries_boundries.py
+++ bbox_world_countries_boundries.py
@@ -1,5 +1,22 @@
 from osgeo import ogr, osr
-import shutil, os, os.path 
+import shutil, os, os.path, math
+
+def calculateLength(minlong, minlat, maxlong, maxlat):
+    rminlat = math.radians(minlat)
+    rminlong = math.radians(minlong)
+    rmaxlat = math.radians(maxlat)
+    rmaxlong = math.radians(maxlong)
+    dLat = rmaxlat - rminlat
+    dLong = rmaxlong - rminlong
+    a = math.sin(dLat/2)**2 + math.cos(rminlat) * math.cos(rmaxlat) \
+    * math.sin(dLong/2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    distance = 6371 * c
+    return distance
+
+def calculateArea(minlong, minlat, maxlong, maxlat):
+    area = calculateLength(minlong, minlat, maxlong, minlat) * calculateLength(maxlong, minlat, maxlong, maxlat)
+    return area
 
 #World countries boundries shapefile
 srcFile = ogr.Open("/home/zia/Documents/Data/TM_WORLD_BORDERS-0.3/TM_WORLD_BORDERS.shp")
@@ -21,8 +38,13 @@ dstPath = os.path.join('/home/zia/bounding-box', 'bounding-boxes.shp')
 dstFile = driver.CreateDataSource(dstPath)
 dstLayer = dstFile.CreateLayer('layer', spatialRef)
 
-#Make a field for the new shapefile
+#Make a Name field for the new shapefile
 fieldDef = ogr.FieldDefn('Country', ogr.OFTString)
+fieldDef.SetWidth(50)
+dstLayer.CreateField(fieldDef)
+
+#Make an area field
+fieldDef = ogr.FieldDefn('Area', ogr.OFTReal)
 fieldDef.SetWidth(50)
 dstLayer.CreateField(fieldDef)
 
@@ -45,10 +67,14 @@ for i in range(layer.GetFeatureCount()):
     polygon = ogr.Geometry(ogr.wkbPolygon)
     polygon.AddGeometry(linearRing)
     
+    #Calculate area using Haversine Formula
+    area = calculateArea(minlong, minlat, maxlong, maxlat)
+    
     #Feed to the new shapefile both polygon and country name attribute
     feature = ogr.Feature(dstLayer.GetLayerDefn())
     feature.SetGeometry(polygon)
     feature.SetField('Country', countryName)
+    feature.SetField('Area', area)
     dstLayer.CreateFeature(feature)
     
     #Destroy the feature for subsequents loop
@@ -57,5 +83,3 @@ for i in range(layer.GetFeatureCount()):
 #Destroy both the source and destination shapefiles. Destinations' destruction is necessary for saving ensurement    
 srcFile.Destroy()
 dstFile.Destroy()
-    
-    
